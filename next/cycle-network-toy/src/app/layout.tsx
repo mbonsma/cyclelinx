ff1b01be70cfd8b1c73fb1059e8eb2fc16cc190d
cklamann <12862284+cklamann@users.noreply.github.com>
2024-01-16 15:59:38 -0500

    render 40 budget

--- next/cycle-network-toy/src/app/layout.tsx
+++ next/cycle-network-toy/src/app/layout.tsx
@@ -22,6 +22,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+          crossOrigin=""
+        />
+      </head>
       <body className={inter.className}>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
--- next/cycle-network-toy/src/app/page.tsx
+++ next/cycle-network-toy/src/app/page.tsx
@@ -1,11 +1,32 @@
-import { Button, Grid } from "@mui/material";
+"use client";
+
+import { Grid } from "@mui/material";
+import styled from "@emotion/styled";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import { LatLng, LatLngBounds } from "leaflet";
+import { GeoJsonObject } from "geojson";
+import budget40 from "../../public/budget_40.json";
 
 export default function Home() {
+  const c1 = new LatLng(43.6, -79.3);
+  const c2 = new LatLng(43.7, -79.4);
   return (
     <main>
       <Grid>
-        <Button color="primary">Click me</Button>
+        <StyledLeafletContainer
+          bounds={new LatLngBounds(c1, c2)}
+          zoom={12}
+          scrollWheelZoom={true}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <GeoJSON data={budget40 as GeoJsonObject} />
+        </StyledLeafletContainer>
       </Grid>
     </main>
   );
 }
+
+const StyledLeafletContainer = styled(MapContainer)`
+  width: 100%;
+  height: 100vh;
+`;
